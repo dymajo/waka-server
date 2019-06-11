4946fd67a21f82b6c280b0e39e7ef180d25b152c
Matt Davidson <matt.davidson@axessadvisory.com>
2019-06-11 16:18:01 +1000

    implement au-syd vehicles

--- src/waka-worker/realtime/regions/au-syd.ts
+++ src/waka-worker/realtime/regions/au-syd.ts
@@ -1,17 +1,20 @@
 // import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
 import axios from 'axios'
 import * as protobuf from 'protobufjs'
-import BaseRealtime from './BaseRealtime'
-import Connection from '../../db/connection'
 import * as Logger from 'bunyan'
+import { Response, Request } from 'express'
+import { VarChar } from 'mssql'
+import Connection from '../../db/connection'
 import {
   PositionFeedMessage,
   UpdateFeedMessage,
   TripUpdate,
   VehiclePosition,
-} from './types'
-import { Response, Request } from 'express'
-import { VarChar } from 'mssql'
+  BaseRealtime,
+  WakaRequest,
+  PositionFeedEntity,
+} from '../../../typings'
+
 const schedulePullTimeout = 20000
 const scheduleLocationPullTimeout = 15000
 
@@ -24,7 +27,7 @@ const modes = [
   'sydneytrains',
 ]
 
-interface IRealtimeAUSYDProps {
+interface RealtimeAUSYDProps {
   apiKey: string
   connection: Connection
   logger: Logger
@@ -38,11 +41,11 @@ class RealtimeAUSYD extends BaseRealtime {
   lastVehicleUpdate: any
   currentData: { [tripId: string]: TripUpdate }
   currentDataFails: number
-  currentVehicleData: { [tripId: string]: VehiclePosition }
+  currentVehicleData: PositionFeedEntity[]
   currentVehicleDataFails: any
   tripUpdateOptions: { url: string; headers: { Authorization: any } }
   vehicleLocationOptions: { url: string; headers: { Authorization: any } }
-  constructor(props: IRealtimeAUSYDProps) {
+  constructor(props: RealtimeAUSYDProps) {
     super()
     const { apiKey, connection, logger } = props
     this.connection = connection
@@ -53,7 +56,7 @@ class RealtimeAUSYD extends BaseRealtime {
     this.lastVehicleUpdate = null
     this.currentData = {}
     this.currentDataFails = 0
-    this.currentVehicleData = {}
+    this.currentVehicleData = []
     this.currentVehicleDataFails = null
 
     this.tripUpdateOptions = {
@@ -102,6 +105,7 @@ class RealtimeAUSYD extends BaseRealtime {
           const feed = _feed as UpdateFeedMessage
 
           // const feed = GtfsRealtimeBindings.TripUpdate.decode(buffer)
+
           feed.entity.forEach(trip => {
             if (trip.tripUpdate) {
               newData[trip.tripUpdate.trip.tripId] = trip.tripUpdate
@@ -124,7 +128,7 @@ class RealtimeAUSYD extends BaseRealtime {
     const { logger, vehicleLocationOptions } = this
     const root = await protobuf.load('tfnsw-gtfs-realtime.proto')
     const FeedMessage = root.lookupType('transit_realtime.FeedMessage')
-    const newVehicleData = {}
+    let newVehicleData: PositionFeedEntity[] = []
     const results = await Promise.all(
       modes.map(async mode => {
         try {
@@ -136,11 +140,15 @@ class RealtimeAUSYD extends BaseRealtime {
           const _feed = FeedMessage.decode(uInt8) as unknown
           // const _feed = GtfsRealtimeBindings.FeedMessage.decode(res)
           const feed = _feed as PositionFeedMessage
-          feed.entity.forEach(trip => {
-            if (trip.vehicle) {
-              newVehicleData[trip.vehicle.trip.tripId] = trip.vehicle
-            }
-          })
+          newVehicleData = newVehicleData.concat(feed.entity)
+          // feed.entity.forEach(trip => {
+          //   if (trip.vehicle.trip.routeId === '2441_343') {
+          //     console.log(trip)
+          //   }
+          //   if (trip.vehicle) {
+          //     newVehicleData[trip.vehicle.trip.tripId] = trip.vehicle
+          //   }
+          // })
         } catch (err) {
           // console.error(err)
         }
@@ -151,56 +159,63 @@ class RealtimeAUSYD extends BaseRealtime {
     this.lastVehicleUpdate = new Date()
   }
 
-  async getTripsEndpoint(req: Request, res: Response) {
+  async getTripsEndpoint(
+    req: WakaRequest<{ trips: string[]; stop_id: string }, null>,
+    res: Response
+  ) {
     const { trips, stop_id } = req.body
     const realtimeInfo = {}
     for (const trip in trips) {
-      try {
-        const data = this.currentData[trip]
+      if (Object.prototype.hasOwnProperty.call(trips, trip)) {
+        try {
+          const data = this.currentData[trip]
 
-        if (typeof data !== 'undefined') {
-          const stop = data.stopTimeUpdate.find(stu => stu.stopId === stop_id)
-          const timeUpdate = stop.departure
-          const info = {}
-          Object.assign(
-            info,
-            stop.stopSequence && { stop_sequence: stop.stopSequence },
-            stop.departure && {
-              delay: stop.departure.delay,
-              timestamp: stop.departure.time.toNumber(),
-            }
-          )
-          realtimeInfo[trip] = info
-        }
-      } catch (error) {}
+          if (typeof data !== 'undefined') {
+            const stop = data.stopTimeUpdate.find(stu => stu.stopId === stop_id)
+            const timeUpdate = stop.departure
+            const info = {}
+            Object.assign(
+              info,
+              stop.stopSequence && { stop_sequence: stop.stopSequence },
+              stop.departure && {
+                delay: stop.departure.delay,
+                timestamp: stop.departure.time.toNumber(),
+              }
+            )
+            realtimeInfo[trip] = info
+          }
+        } catch (error) {}
+      }
     }
     return res.send(realtimeInfo)
   }
 
-  async getVehicleLocationEndpoint(req, res) {
+  async getVehicleLocationEndpoint(
+    req: WakaRequest<{ trips: string[] }, null>,
+    res: Response
+  ) {
     const { logger, currentVehicleData } = this
     const { trips } = req.body
     const vehicleInfo = {}
     for (const trip in trips) {
-      try {
-        const data = currentVehicleData[trip]
-        vehicleInfo[trip] = {
-          latitude: data.position.latitude,
-          longitude: data.position.longitude,
-        }
-      } catch (err) {}
+      if (Object.prototype.hasOwnProperty.call(trips, trip)) {
+        const element = trips[trip]
+        try {
+          const data = currentVehicleData[trip]
+          vehicleInfo[trip] = {
+            latitude: data.position.latitude,
+            longitude: data.position.longitude,
+          }
+        } catch (err) {}
+      }
     }
-    debugger
     res.send(vehicleInfo)
   }
 
-  async getLocationsForLine(req, res) {
-    debugger
+  async getLocationsForLine(req: WakaRequest<null, { line: string }>, res) {
     const { logger, connection } = this
     const { line } = req.params
-    const currentTripIds = Object.keys(this.currentVehicleData)
-    debugger
-    if (currentTripIds.length === 0) {
+    if (this.currentVehicleData.length === 0) {
       return res.send([])
     }
 
@@ -209,28 +224,26 @@ class RealtimeAUSYD extends BaseRealtime {
       sqlRouteIdRequest.input('route_short_name', VarChar(50), line)
       const routeIdResult = await sqlRouteIdRequest.query<{ route_id: string }>(
         `
-        SELECT route_id
-        FROM routes
-        WHERE route_short_name = @route_short_name
-        `
+      SELECT route_id
+      FROM routes
+      WHERE route_short_name = @route_short_name
+      `
       )
-
       const routeIds = routeIdResult.recordset.map(r => r.route_id)
-      const tripIds = currentTripIds.filter(tripId => {
-        const routeId = this.currentVehicleData[tripId].trip.routeId
-        routeIds.includes(routeId)
-      })
-      // const tripIds = trips.map(entity => entity.vehicle.trip.trip_id)
+      const trips = this.currentVehicleData.filter(entity =>
+        routeIds.includes(entity.vehicle.trip.routeId)
+      )
+      const tripIds = trips.map(entity => entity.vehicle.trip.tripId)
       const escapedTripIds = `'${tripIds.join("', '")}'`
       const sqlTripIdRequest = connection.get().request()
       const tripIdRequest = await sqlTripIdRequest.query<{
         trip_id: string
         direction_id: number
       }>(`
-        SELECT *
-        FROM trips
-        WHERE trip_id IN (${escapedTripIds})
-        `)
+      SELECT *
+      FROM trips
+      WHERE trip_id IN (${escapedTripIds})
+      `)
 
       const tripIdsMap = {}
       tripIdRequest.recordset.forEach(
@@ -241,10 +254,8 @@ class RealtimeAUSYD extends BaseRealtime {
       const result = trips.map(entity => ({
         latitude: entity.vehicle.position.latitude,
         longitude: entity.vehicle.position.longitude,
-        bearing: entity.vehicle.position.bearing
-          ? parseInt(entity.vehicle.position.bearing, 10)
-          : null,
-        direction: tripIdsMap[entity.vehicle.trip.trip_id],
+        bearing: entity.vehicle.position.bearing,
+        direction: tripIdsMap[entity.vehicle.trip.tripId],
         updatedAt: this.lastVehicleUpdate,
       }))
       res.send(result)
