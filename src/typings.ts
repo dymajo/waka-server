f0f7902b1b281e424b718b07c961e4f9dd2baddd
Matt Davidson <matt.davidson@axessadvisory.com>
2019-06-13 10:50:52 +1000

    add function types for base realtime

--- src/typings.ts
+++ src/typings.ts
@@ -1,6 +1,6 @@
 import Long from 'long'
 import * as Logger from 'bunyan'
-import { Request, Response } from 'express'
+import { Request, Response, Router } from 'express'
 import { config as SqlConfig } from 'mssql'
 import Connection from './waka-worker/db/connection'
 import GatewayLocal from './waka-orchestrator/adaptors/gatewayLocal'
@@ -103,9 +103,43 @@ export abstract class BaseKeyvalue {
 export abstract class BaseRealtime {
   connection: Connection
   logger: Logger
+  lastUpdate: Date
+  getTripsCached?(
+    trips: string[]
+  ): {
+    [tripId: string]: {
+      stop_sequence: number
+      delay: number
+      timestamp: number
+      v_id: number
+      double_decker: boolean
+      ev: boolean
+    }
+  }
   scheduleLocationPull?(): Promise<void>
   schedulePull?(): Promise<void>
   abstract start(): void
+  abstract stop(): void
+  abstract getVehicleLocationEndpoint(
+    req: WakaRequest<{ trips: string[] }, null>,
+    res: Response
+  ): Promise<Response>
+  abstract getLocationsForLine(
+    req: WakaRequest<null, { line: string }>,
+    res: Response
+  ): Promise<Response>
+  abstract getTripsEndpoint(
+    req: WakaRequest<{ trips: string[] }, null>,
+    res: Response
+  ): Promise<Response>
+  abstract getTripsEndpoint(
+    req: WakaRequest<{ trips: string[]; train: boolean }, null>,
+    res: Response
+  ): Promise<Response>
+  abstract getTripsEndpoint(
+    req: WakaRequest<{ stop_id: string }, null>,
+    res: Response
+  )
 }
 
 export abstract class BaseLines {
