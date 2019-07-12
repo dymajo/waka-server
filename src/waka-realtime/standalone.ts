6b932b41be28942c8cb718fb8fadf24c048dbaff
Matt Davidson <matt@mattdavidson.kiwi>
2019-07-12 17:53:34 +1000

    add apikey spaceholder back in

--- src/waka-realtime/standalone.ts
+++ src/waka-realtime/standalone.ts
@@ -7,15 +7,15 @@ import EnvMapper from '../envMapper'
 // const envMapper = new EnvMapper()
 // const config = envMapper.fromEnvironmental(process.env)
 const config = {
-  prefix: 'nz-akl',
-  version: '20190710',
-  quota: {
-    interval: 1000,
-    rate: 5,
-    concurrency: 5,
-  },
-  api: { '' },
-}
+    prefix: 'nz-akl',
+    version: '20190710',
+    quota: {
+      interval: 1000,
+      rate: 5,
+      concurrency: 5,
+    },
+    api: { 'nz-akl': '' },
+  }
   // const config = {
   //   prefix: 'au-syd',
   //   version: '20190710',
@@ -26,7 +26,7 @@ const config = {
   //   },
   //   api: { 'au-syd': '' },
   // }
-  ; (async () => {
-    const realtime = new WakaRealtime(config)
-    realtime.start()
-  })()
+;(async () => {
+  const realtime = new WakaRealtime(config)
+  realtime.start()
+})()
